2fc3854d322e9a2791d7eba4e955a8a9c0a35396
visig <visig@protonmail.ch>
2018-12-05 17:53:51 +0800

    remove unused package "strescape"

--- strescape/strescape.go
+++
@@ -1,9 +0,0 @@
-// Package strescape is utility tool to escape strings.
-package strescape
-
-import "strings"
-
-// SingleQuote convert all "'" -> "\\'".
-func SingleQuote(str string) string {
-	return strings.Replace(str, "'", "\\'", -1)
-}
